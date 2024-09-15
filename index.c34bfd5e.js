c7a3850baac3ff547d903e56fd05f4572280e62d
Tetyana Chuhai <chugaytatyana@gmail.&git config --global user.email chugaytatyana@gmail.>
2024-09-15 15:33:47 +0300

    make build

---
+++ index.c34bfd5e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.top=e.clientHeight/2-t.offsetHeight/2+"px",t.style.left=e.clientWidth/2-t.offsetWidth/2+"px";
+//# sourceMappingURL=index.c34bfd5e.js.map
